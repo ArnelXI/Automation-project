88374a2838532dd6aa16fd4700ee73ad54b497d7
Yv-Arnel <jeanbernard.yvarnel@gmail.com>
2023-12-10 18:04:15 -0500

    small bug to fix

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
 
-#going through every file in the download Directory
+#going through every file in the Directories: Desktop, Documents, Download
 
 list_directory = ["Desktop","Documents","Download"]
 
+#Photo File Formats: JPEG, GIF, PNG, TIFF, BMP.
+#Music File Formats: AAC, MP3, WAV, WMA, DOLBY® DIGITAL, DTS.
 for directory in list_directory:
     for file in os.listdir(directory):
         filename  = os.fsdecode(file)
